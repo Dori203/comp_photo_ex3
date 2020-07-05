fecfd65c53e0d23751373bb6d3ba760ca0848aa9
mosem <mandelmoshe@gmail.com>
2020-07-05 18:02:35 +0300

    fixing positional argument error

--- colorization.py
+++ colorization.py
@@ -19,7 +19,7 @@ STYLEGAN_MODEL_URL = 'https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBB
 
 def convert_to_greyscale(rgb):
     print("rgb shape: {}".format(rgb.shape))
-    return np.dot(rgb, [0.2126, 0.7152, 0.0722])
+    return np.dot(rgb[-1,:], [0.2126, 0.7152, 0.0722])
 
 
 def optimize_latent_codes(args):
