d01b995a69effe6e00ce08dc357f668d1a817bc8
dori203 <dori203@cs.huji.ac.il>
2020-07-07 16:13:24 +0300

    deblur_3.py: add blur kernel to NN, same as mask.

--- deblur_3.py
+++ deblur_3.py
@@ -37,6 +37,38 @@ def generate_random_mask(img_shape, mask_size, kernel_size, alpha):
     blurred_mask = add_motion_blur_single_image_grayscale(mask_2d, kernel_size, alpha)
     return blurred_mask[..., np.newaxis]
 
+def motion_blur_kernel_1D(kernel_size, angle):
+    """Returns a 2D image kernel for motion blur effect.
+    Arguments:
+    kernel_size -- the height and width of the kernel. Controls strength of blur.
+    angle -- angle in the range [0, np.pi) for the direction of the motion.
+    """
+    if kernel_size % 2 == 0:
+        raise ValueError('kernel_size must be an odd number!')
+    if angle < 0 or angle > np.pi:
+        raise ValueError('angle must be between 0 (including) and pi (not including)')
+    norm_angle = 2.0 * angle / np.pi
+    if norm_angle > 1:
+        norm_angle = 1 - norm_angle
+    half_size = kernel_size // 2
+    if abs(norm_angle) == 1:
+        p1 = (half_size, 0)
+        p2 = (half_size, kernel_size-1)
+    else:
+        alpha = np.tan(np.pi * 0.5 * norm_angle)
+        if abs(norm_angle) <= 0.5:
+            p1 = (2*half_size, half_size - int(round(alpha * half_size)))
+            p2 = (kernel_size-1 - p1[0], kernel_size-1 - p1[1])
+        else:
+            alpha = np.tan(np.pi * 0.5 * (1-norm_angle))
+            p1 = (half_size - int(round(alpha * half_size)), 2*half_size)
+            p2 = (kernel_size - 1 - p1[0], kernel_size-1 - p1[1])
+    rr, cc = line(p1[0], p1[1], p2[0], p2[1])
+    kernel = np.zeros((kernel_size, kernel_size), dtype=np.float32)
+    kernel[rr, cc] = 1.0
+    kernel /= kernel.sum()
+    return kernel
+
 def motion_blur_kernel(kernel_size, angle):
     """Returns a 2D image kernel for motion blur effect.
     Arguments:
@@ -106,7 +138,7 @@ def add_motion_blur_single_image(image, kernel_size, angle):
     :return:
     """
     # get kernel
-    kernel = motion_blur_kernel(kernel_size, angle)
+    kernel = motion_blur_kernel_1D(kernel_size, angle)
 
     # convolve image.
     for i in range(3):
