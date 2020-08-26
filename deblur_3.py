42312a87a5d686abfbbeb3be88c360698b89bf44
dori203 <dori203@cs.huji.ac.il>
2020-08-26 20:52:58 +0300

    Trying to lead the search with a goal.

--- deblur_3.py
+++ deblur_3.py
@@ -252,9 +252,103 @@ def optimize_latent_codes(args):
         # # imageio.imwrite(os.path.join(args.restorations_dir, "latent_1.png"), latent_2)
 
 
+def optimize_latent_codes_source_target(args, source_latent_code):
+    #tf.enable_eager_execution()
+    tflib.init_tf()
+    with dnnlib.util.open_url(STYLEGAN_MODEL_URL, cache_dir=config.cache_dir) as f:
+        _G, _D, Gs = pickle.load(f)
+
+    latent_code = tf.get_variable(
+        name='latent_code', shape=(1, 18, 512), dtype='float32', initializer=tf.initializers.constant(source_latent_code)
+    )
+    generated_img = Gs.components.synthesis.get_output_for(latent_code, randomize_noise=False)
+    generated_img = tf.transpose(generated_img, [0, 2, 3, 1])
+    generated_img = ((generated_img + 1) / 2) * 255
+
+    original_img = tf.placeholder(tf.float32, [None, args.input_img_size[0], args.input_img_size[1], 3])
+    blur_kernel = tf.placeholder(tf.float32, [None, args.blur_parameters[0], args.blur_parameters[0], 3])
+
+    degraded_img_resized_for_perceptual = tf.image.resize_images(
+        add_motion_blur(original_img,blur_kernel), tuple(args.perceptual_img_size), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR
+    )
+
+
+    generated_img_resized_to_original = tf.image.resize_images(
+        generated_img, tuple(args.input_img_size), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR
+    )
+
+
+    generated_img_resized_for_perceptual = tf.image.resize_images(
+        add_motion_blur(generated_img_resized_to_original, blur_kernel), tuple(args.perceptual_img_size),
+        method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+
+
+
+    generated_img_for_display = tf.saturate_cast(generated_img_resized_to_original, tf.uint8)
+
+
+    perceptual_model = PerceptualModel(img_size=args.perceptual_img_size)
+    generated_img_features = perceptual_model(generated_img_resized_for_perceptual)
+
+    target_img_features = perceptual_model(degraded_img_resized_for_perceptual)
+
+    loss_op = tf.reduce_mean(tf.abs(generated_img_features - target_img_features))
+
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=args.learning_rate)
+    train_op = optimizer.minimize(loss_op, var_list=[latent_code])
+
+    sess = tf.get_default_session()
+
+    img_names = sorted(os.listdir(args.imgs_dir_second))
+    for img_name in img_names:
+        img = imageio.imread(os.path.join(args.imgs_dir, img_name))
+        img = cv2.resize(img, dsize=tuple(args.input_img_size))
+        blur_kernel_3d = motion_blur_kernel(args.blur_parameters[0], args.blur_parameters[1])
+        corrupted_img = add_motion_blur_single_image(img,args.blur_parameters[0],args.blur_parameters[1])
+
+        imageio.imwrite(os.path.join(args.corruptions_dir, img_name), corrupted_img)
+        #imageio.imwrite(os.path.join(args.masks_dir, img_name), mask * 255)
+
+        sess.run(tf.variables_initializer([latent_code] + optimizer.variables()))
+
+        progress_bar_iterator = tqdm(
+            iterable=range(args.total_iterations),
+            bar_format='{desc}: {percentage:3.0f}% |{bar}| {n_fmt}/{total_fmt}{postfix}',
+            desc=img_name
+        )
+
+        for i in progress_bar_iterator:
+            loss, _ = sess.run(
+                fetches=[loss_op, train_op],
+                feed_dict={
+                    original_img: img[np.newaxis, ...],
+                    blur_kernel: blur_kernel_3d[np.newaxis, ...]
+                }
+            )
+
+            progress_bar_iterator.set_postfix_str('loss=%.2f' % loss)
+
+        reconstructed_imgs, latent_codes = sess.run(
+            fetches=[generated_img_for_display, latent_code],
+            feed_dict={
+                original_img: img[np.newaxis, ...],
+                blur_kernel: blur_kernel_3d[np.newaxis, ...]
+            }
+        )
+
+        imageio.imwrite(os.path.join(args.restorations_dir, img_name), reconstructed_imgs[0])
+        np.savez(file=os.path.join(args.latents_dir, img_name + '.npz'), latent_code=latent_codes[0])
+
+
+        return latent_codes[0]
+
+
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--imgs-dir', type=str, required=True)
+    parser.add_argument('--imgs-dir-second', type=str, required=True)
     parser.add_argument('--masks-dir', type=str, required=True)
     parser.add_argument('--corruptions-dir', type=str, required=True)
     parser.add_argument('--restorations-dir', type=str, required=True)
@@ -266,6 +360,7 @@ if __name__ == '__main__':
     parser.add_argument('--blur-parameters', type=int, nargs=2, default=(51, 1))
     parser.add_argument('--learning-rate', type=float, default=1e-2)
     parser.add_argument('--total-iterations', type=int, default=100)
+    parser.add_argument('--source', type=int, default=1)
 
     args = parser.parse_args()
 
@@ -278,12 +373,15 @@ if __name__ == '__main__':
 
     print("latent code shape is: ", latent_code.shape)
     latent_code = latent_code.reshape((1, 18, 512))
-    latent_zeros = np.zeros(latent_code.shape)
-    latent_interp = (latent_code + latent_zeros)/2
+
+    second = optimize_latent_codes_source_target(args, latent_code)
+    second = second.reshape((1, 18, 512))
+
+    latent_interp = (latent_code + second)/2
     print("latent code shape after reshape is: ", latent_code.shape)
     #tf.reset_default_graph()
     get_image_from_latant_code(latent_code, "latent_0.png")
-    get_image_from_latant_code(latent_zeros, "latent_zeros.png")
+    get_image_from_latant_code(second, "latent_zeros.png")
     get_image_from_latant_code(latent_interp, "latent_interp.png")
 
     # #print("latent code value is: ", latent_code)
