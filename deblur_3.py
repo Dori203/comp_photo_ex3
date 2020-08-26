0381d4fcc255ca033354f0bba48b2c28413c1219
dori203 <dori203@cs.huji.ac.il>
2020-08-26 17:06:08 +0300

    Try saving an image.

--- deblur_3.py
+++ deblur_3.py
@@ -134,6 +134,7 @@ def get_image_from_latant_code(latent_code):
 
 def optimize_latent_codes(args):
     tflib.init_tf()
+    tf.enable_eager_execution()
     with dnnlib.util.open_url(STYLEGAN_MODEL_URL, cache_dir=config.cache_dir) as f:
         _G, _D, Gs = pickle.load(f)
 
@@ -230,7 +231,6 @@ def optimize_latent_codes(args):
 
 
 if __name__ == '__main__':
-    tf.enable_eager_execution()
     parser = argparse.ArgumentParser()
     parser.add_argument('--imgs-dir', type=str, required=True)
     parser.add_argument('--masks-dir', type=str, required=True)
